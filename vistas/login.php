00a1f2d23a8cb6f5a27ca7f656857cdeebc49f5a
aaronpolo1000 <aaronpolo1000@gmail.com>
2023-05-04 16:30:20 -0300

    a

---
+++ vistas/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesion</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+<form action="" method="post">
+    <list>
+        <input type="text"><label for="">Usuario</label>
+  
+        <input type="password"><label for="">Contraseña</label>
+<br>
+       <label for="">Mantener Sesion</label> <input type="checkbox" name="" id="">
+  
+        <input type="submit">
+  
+    </list>
+</form>
+</body>
+</html>
