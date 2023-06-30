29ba4eedb594895c2a93a8e27991b5dfe834a43b
OGBiggieCheese <manuel.casimiro.et26@gmail.com>
2023-06-30 13:53:52 -0300

    agrego tablas

--- views/layout.php
+++ views/layout.php
@@ -24,6 +24,192 @@
   $section = (isset($section)) ? $section : 'home';
   require_once $section;
   ?>
-</body>
-</html>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Lunes</th>
+      <th scope="col">Martes</th>
+      <th scope="col">Miercoles</th>
+      <th scope="col">Jueves</th>
+      <th scope="col">Viernes</th>
+      <th scope="col">Sabado</th>
+      <th scope="col">Domingo</th>
+     
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>    
+    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
 
+   
