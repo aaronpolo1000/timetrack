87c3d7aabd0d0d0468c5aa74c00aa5b6f2c4a8ea
aaronpolo1000 <aaronpolo1000@gmail.com>
2023-10-20 09:34:21 -0300

    a

---
+++ views/home.php
@@ -0,0 +1,187 @@
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Lunes</th>
+      <th scope="col">Martes</th>
+      <th scope="col">Miercoles</th>
+      <th scope="col">Jueves</th>
+      <th scope="col">Viernes</th>
+      <th scope="col">Sabado</th>
+      <th scope="col">Domingo</th>
+     
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>    
+    <th scope="row">1</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td ></td>
+      <td></td>
+      <td></td>
+      <td ></td>
+      <td></td>
+      <td></td>
+      
+    </tr>
